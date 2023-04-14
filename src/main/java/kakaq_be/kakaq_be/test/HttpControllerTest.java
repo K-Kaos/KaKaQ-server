7932c56f74d8af122e0c96a64e6de2f00cbb08b7
Esoh124 <75177296+Esoh124@users.noreply.github.com>
2023-04-14 10:21:03 +0900

    chatgptAPI_added

--- src/main/java/kakaq_be/kakaq_be/test/HttpControllerTest.java
+++ src/main/java/kakaq_be/kakaq_be/test/HttpControllerTest.java
@@ -1,10 +1,16 @@
 package kakaq_be.kakaq_be.test;
 
+import jakarta.servlet.http.HttpServletRequest;
 import kakaq_be.kakaq_be.model.User;
 import kakaq_be.kakaq_be.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.MediaType;
 import java.util.List;
 import java.util.Objects;
 
@@ -31,15 +37,37 @@ public class HttpControllerTest {
         userRepository.save(new_user);
         return 0;
     }
+    String gpt_API_KEY = "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";
 
+    @GetMapping("/http/topic")
+//    @SendTo("/topic/public")
+    public String sendTopic(HttpServletRequest param) {
+        System.out.println(param);
+        String topic = param.getParameter("topic");
+        //api에 요청 헤더
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", "Bearer " + gpt_API_KEY);
+        //이 아래 url로 모델 바꿀수 있음.
+        String url = "https://api.openai.com/v1/engines/text-davinci-003/completions";
 
-//    @RequestMapping("/http/login")
-//    public int loginTest(@RequestBody User temp){
-//        System.out.println(temp);
-//        User temp_user = new User(temp.getEmail(), temp.getPassword());
-//        if(userRepository.ExistsByUserEmailANDUserPassword(temp.getEmail(), temp.getPassword())) return 0;
-//        else return 1;
-//    }
+        // 보낼 본문 데이터 설정
+        System.out.println(topic);
+        String prompt = topic + "에 대한 설문조사 질문지 폼을 객관식 답변용으로만 5개 만들어줘";
+        System.out.println(prompt);
+        String requestBody = "{\"prompt\":\"" + prompt + "\",\"max_tokens\":4000,\"temperature\":0.7}";
+        HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
+
+        // API에 요청
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, request, String.class);
+//        ChatMessage responseChat = new ChatMessage();
+        System.out.println(response.getBody());
+
+        // API 응답 결과 이거 format 만들어서 맞게 만들어야 함.
+        return response.getBody();
+    }
+
 
     @PutMapping("/http/put")
     public String putTest() {
