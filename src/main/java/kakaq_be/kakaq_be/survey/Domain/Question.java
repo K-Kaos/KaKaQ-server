1320b027dbfa8bbb5a5813b4f297903a91573f7c
yeon409 <yeon000409@gmail.com>
2023-05-04 09:50:43 +0900

    feat:survey question

--- src/main/java/kakaq_be/kakaq_be/survey/Domain/Question.java
+++ src/main/java/kakaq_be/kakaq_be/survey/Domain/Question.java
@@ -17,21 +17,35 @@ import java.util.List;
 public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private Long question_id;
 
     @Column(nullable = false, length = 50)
     private String text;
 
     @ManyToOne
+    @JoinColumn(name = "question_type_id", nullable = false)
     private QuestionType type;
+
+//    @Column(nullable = false)
+//    private String type;
 
     @ElementCollection
     private List<String> options;
 
     @ManyToOne
+    @JoinColumn(name = "survey_id", nullable = false)
     private Survey survey;
 
     @OneToMany(mappedBy = "question")
     private List<Response> responses;
+
+    @Builder
+    public Question(Long question_id, String text, QuestionType type,List<String> options, Survey survey){
+        this.question_id = question_id;
+        this.text = text;
+        this.type = type;
+        this.options = options;
+        this.survey = survey;
+    }
 
 }
